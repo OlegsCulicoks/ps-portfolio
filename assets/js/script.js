67610e51d1b68c8d9638a8f806903a55008685d2
OlegsCulicoks <ipa21.o.culicoks@vtdt.edu.lv>
2024-09-17 14:07:47 +0300

    hfhf

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav ul li a');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -11,10 +12,10 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav ul li a[href*=' + id + ' ]' ).classList.add(active)
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
             })
         }
     })
@@ -23,4 +24,15 @@ window.onscroll = () => {
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
+}
+
+function sendMail() {
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_0yirtxt","template_iczogjr",parms).then(alert("Email Sent!!"))
 }
